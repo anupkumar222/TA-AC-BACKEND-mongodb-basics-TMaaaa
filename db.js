f97fc660aa5d088fda48fdf91da3c857f10e8752
anup kumar <kanup414@gmail.com>
2022-12-10 15:37:18 +0530

    done basics

---
+++ db.js
@@ -0,0 +1,20 @@
+var data = {name: 'Dhauldhar range', height: '4000 mtrs'}
+
+var hills = [
+    {
+        name: 'Dhauldhar range',
+        height: '4000 mtrs'
+    },
+    {
+        name: 'nandi range',
+        height: '7000 mtrs'
+    },
+    {
+        name: 'Himalyan range',
+        height: '7500 mtrs'
+    },
+    {
+        name: 'kailash range',
+        height: '4000 mtrs'
+    }
+]
